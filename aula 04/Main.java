456eafe49c04b75114df81583e9e755b8796fa9f
suelendias2 <165045573+suelendias2@users.noreply.github.com>
2024-09-20 09:08:01 -0300

    Add files via upload

--- aula 04/Main.java
+++ aula 04/Main.java
@@ -2,38 +2,20 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner (System.in);
+    System.out.print("Digite algo:");
+    String entrada = scanner.nextLine();
 
-    Double num1;
-    Double num2;
-    String operacao;
-
-    System.out.println("Digite o primeiro numero: ");
-    num1 = scanner.nextDouble();
-
-    System.out.println("Digite a operacao(+, - , /, *");
-    operacao = scanner.next();
-
-    System.out.println("Digite o segundo numero:");
-    num2 = scanner.nextDouble();
-
-    // Realizando a operacao escolhida pelo metodo calculo
-    System.out.println("Resultado:" + calculo(num1, operacao, num2));
-  }
-
-  public static Double calculo(Double num1, String operacao, Double num2) {
-    Double resposta = 0.0;
-
-    if (operacao.equals("+")) {
-      resposta = num1 + num2;
-    }else if(operacao.equals("-")){
-      resposta = num1 - num2;
-    }else if(operacao.equals("*")){
-      resposta = num1 * num2;
-    }else if(operacao.equals("/")){
-      resposta = num1 / num2;
+    //Tenta converter a entrada para um numero inteiro
+    try{
+      int numero = Integer.parseInt(entrada);
+      System.out.println("voce digitou um numero inteiro:" + numero);
+    } catch (NumberFormatException e){
+      System.out.println("voce digitou uma string:" + entrada);
     }
-    return resposta;
+
+    scanner.close();
   }
+
 
 }
